6dd41a32b552d35013482d5f3d41b4203afaa8d6
Arushay-Shahid <ummearushay567@gmail.com>
2024-11-05 20:58:30 +0500

    Add files via upload

---
+++ Tuple in python/main.py
@@ -0,0 +1,23 @@
+#Empty tuple
+my_tuple=()
+print(my_tuple)
+#tuple having integers
+my_tuple=(1,2,3)
+print(my_tuple)
+#tuple with mixed numbers
+my_tuple=(1,"Hello", 3.4)
+print(my_tuple)
+#nested tuple
+my_tuple=("mouse",[8,4,6],[1,2,3])
+print(my_tuple)
+#accessing tuple elements
+my_tuple('p','e','r','m','t','i')
+print(my_tuple)
+#nested tuple
+my_tuple=("mouse",[8,4,6],91,2,30)
+print(my_tuple)
+#nested index
+n_tuple=("mouse", [8,4,6],[1,2,3])
+#nested index
+print(n ,tuple[0][3])
+
